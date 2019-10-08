f188801d390659797e79e4a0624c8d033c160330
Icaro Gavazza <icarodgl@gmail.com>
2019-10-08 18:57:55 -0300

    feat(selection): create selection sort

---
+++ src/selection.py
@@ -0,0 +1,9 @@
+
+def selection(vector):
+    for index in range(len(vector)):
+        minor = index
+        for next in range(index+1, len(vector)):
+            if vector[minor] > vector[next]:
+                minor = next
+
+        vector[index], vector[minor] = vector[minor], vector[index]
