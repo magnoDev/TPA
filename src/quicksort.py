b3ed9fa95e074f3b041def4060b8fc46b8e73a89
Jennifer <jennifergamaral@gmail.com>
2019-10-23 13:13:29 -0300

    QuickSort correto

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,75 +1,29 @@
-def quicksort(v, inicio, fim):
-    i = inicio
-    j = fim
-    p = j//2  #pivô
+def QuickSort(X, IniVet, FimVet):
 
-    while i < j:
-        while v[i] < v[p]:
-            i += 1
-        while v[j] > v[p]:
-            j -= 1
-        if i <= j:
-            #trocar(v[i], v[j])
-            aux = v[i]
-            v[i] = v[j]
-            v[j] = aux
-            i += 1
-            j -= 1
-    if i > j:
-        if inicio < j:
-            quicksort(v, inicio, j)
-        if i < fim:
-            quicksort(v, i, fim)
+        i = IniVet
+        j = FimVet
+        pivo = X[(IniVet + FimVet) // 2]
 
-    return v
+        while (i <= j):
+            while (X[i] < pivo):
+                i = i + 1
+            # fimEnquanto
+            while (X[j] > pivo):
+                j = j - 1
+            # fimEnquanto
+            if (i <= j):
+                aux = X[i]
+                X[i] = X[j]
+                X[j] = aux
+                i = i + 1
+                j = j - 1
+            # fimSe
+            # fimEnquanto
 
-
-# def trocar(valor1, valor2):
-#     aux = valor1
-#     valor1 = valor2
-#     valor = aux
-
-def quicksort(v, inicio, fim):
-    i = inicio
-    j = fim
-    p = fim  # pivô
-
-    if fim - inicio == 1:
-        return v
-
-    while i <= j:
-
-        while v[i] < v[p] and i < fim:
-            i += 1
-        while v[j] > v[p] and j > inicio:
-            j -= 1
-        if i <= j:
-            if j == p:
-                p = i
-
-            aux = v[i]
-            v[i] = v[j]
-            v[j] = aux
-
-            if i < fim:
-                i += 1
-
-            if j > inicio:
-                j -= 1
-
-    if i > j:
-        if inicio < j:
-            quicksort(v, inicio, j)
-        if i < fim:
-            quicksort(v, i, fim)
-
-
-def main():
-    vector = [52, 45, 25, 31, 28, 17, 65, 35, 42, 86]
-    # sel.selection(vector)
-    quicksort(vector, 0, len(vector) - 1)
-    print(vector)
-
-
-if __name__ == '__main__':
-    main()
+            if (IniVet < j):
+                QuickSort(X, IniVet, j)
+            # fimSe
+            if (i < FimVet):
+                QuickSort(X, i, FimVet)
+            # fimSe
+# fimProcedimento
